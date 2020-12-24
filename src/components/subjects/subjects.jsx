82c2ce938b051905620af3f7e6cefd6d169d8e64
ibtg <ibtg0409@gmail.com>
2020-12-24 21:29:34 +0900

    Remove unused useRef

--- src/components/subjects/subjects.jsx
+++ src/components/subjects/subjects.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import Header from '../header/header'
 import Subject from '../subject/subject'
