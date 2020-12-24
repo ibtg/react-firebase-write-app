b089baa170f2ba3753ee18a0b360828a6a4048b7
ibtg <ibtg0409@gmail.com>
2020-12-24 21:22:08 +0900

    Add writingRepository for writing

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.module.css';
 import App from './App';
 import AuthService from './service/auth_service';
+import WritingRepository from './service/writing_repository'
+
 
 const authService = new AuthService();
+const writingRepository = new WritingRepository()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App authService={authService}/>
+    <App authService={authService} writingRepository={writingRepository}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
